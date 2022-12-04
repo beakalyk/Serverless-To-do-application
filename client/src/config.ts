d2834a63519c0e4c626102dc68edcd26885b232b
beakalyk <akaabe@hotmail.com>
2022-12-04 21:38:55 +0300

    Master commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'xz4qf3092m';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-175y1ce04cr1jg7c.us.auth0.com',
+  clientId: 'ql4xxeHMQL51ehHrlwRteCxCNwglUqjG',
+  callbackUrl: 'http://localhost:3000/callback'
+};
